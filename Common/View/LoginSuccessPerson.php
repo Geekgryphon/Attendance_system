08fea5e455c1c66a6c2ccd96803d6846e927b74e
Geekgryphon <geekgryphon@gmail.com>
2024-04-01 06:53:26 +0800

    開新專案

---
+++ Common/View/LoginSuccessPerson.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h6>你好，陌生人!</h6>
+    <form action="">
+        <button type="submit">登出</button>
+    </form>
+</body>
+</html>
---
+++ Manager/View/Nav.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li><a href="">功能1</a></li>
+        <li><a href="">功能2</a></li>
+        <li><a href="">功能3</a></li>
+        <li><a href="">功能4</a></li>
+    </ul>
+</body>
+</html>
