bdd91abec7191cfcccefe324ec2d2aa04858fe89
Geekgryphon <geekgryphon@gmail.com>
2024-04-06 08:55:11 +0800

    展示頁面和部分的功能實作

--- Common/View/WebUIDemo.php
+++ Common/View/WebUIDemo.php
@@ -4,7 +4,83 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style type="">
+    <style type="text/css">
+        #EmployeeID{
+            border: 1px solid #d9d9d9;
+            border-radius: 5px;
+            pading: 20px;
+            height: 25px;
+            font-size: 14px;
+            font-weight: 100;
+        }
+
+        #EmployeeID:focus {
+            outline: none;
+            border:1px solid #52b5f2;
+        }
+
+        #Date{
+            border: 1px solid #d9d9d9;
+            border-radius: 5px;
+            pading: 20px;
+            height: 25px;
+            font-size: 14px;
+            font-weight: 100;
+        }
+
+        #Date:focus {
+            outline: none;
+            border:1px solid #d9a141;
+        }
+
+        #Selection{
+            outline: none;
+            background-color: #ffffff;
+            border: 1px solid #d9d9d9;
+            border-radius: 5px;
+            height: 30px;
+            
+        }
+
+        Button{
+            border: 1px solid #d9d9d9;
+            background-color: #ffffff;
+            height: 30px;
+            line-height: 20px;
+            padding-left: 10px; 
+            padding-right: 10px;
+            border-radius: 5px;
+            text-align: center;
+            transition: all 0.3s ease;
+        }
+
+        #ButtonSubmit:hover{
+            background-color: #52b5f2; 
+            color: white;
+        }
+
+        #ButtonAdd:hover{
+            background-color: #3cd66d; 
+            color: white;
+        }
+
+        #ButtonDelete:hover{
+            background-color: #d43965; 
+            color: white;
+        }
+
+        tr{
+            height:30px;
+        }
+
+        tr:nth-child(2n){
+            background-color: #96b8d6;
+        }
+
+        tr:nth-child(2n+1){
+            background-color: #d9b6d9;
+        }
+
 
     </style>
 </head>
@@ -12,21 +88,123 @@
     <hr>
     搜尋欄
     <form action="">
-        員工編號 <input type="text" name="EmployeeID"><br/>
-        日期 <input type="datetime-local" name="Date" id="Date"><br/>
-        簽到日期 <input type="date" name="SignDate" id="SignDate"><br/>
-        簽到時間 <input type="time" name="SignTime" id="SignTime"><br/>
+        員工編號 <input type="text" name="EmployeeID" id="EmployeeID"><br/><br/>
+        日期 <input type="datetime-local" name="Date" id="Date"><br/><br/>
+        簽到日期 <input type="date" name="SignDate" id="SignDate"><br/><br/>
+        簽到時間 <input type="time" name="SignTime" id="SignTime"><br/><br/>
+
+        <select name="" id="Selection">
+            <option value="">員工1</option>
+            <option value="">員工2</option>
+        </select>
+
+        <button id="ButtonSubmit">送出</button>
+        <button id="ButtonAdd">新增</button>
+        <button id="ButtonDelete">刪除</button>
     </form>
     <hr>
-    table範例
-    <table>
-        <th></th>
+
+    呈現資料庫資料
+
+
+    <table style="width:100%; border-collapse: collapse;">
+        <tbody>
+            <tr style="background-color: #5e5e5e0e;">
+                <td style="width:15%;">員工編號</td>
+                <td style="width:15%;">簽到日期</td>
+                <td style="width:15%;">簽到時間</td>
+                <td style="width:15%;">簽退時間</td>
+                <td style="width:15%;">是否有遲到</td>
+                <td style="width:25%;"></td>
+            </tr>
+            <tr>
+                <td>EP0352</td>
+                <td>2024-04-01</td>
+                <td>07:58</td>
+                <td>17:46</td>
+                <td>
+                ○
+                </td>
+                <td>
+                    <input type="checkbox" name="" id="">
+                </td>
+            </tr>
+            <tr>
+                <td>EP0352</td>
+                <td>2024-04-02</td>
+                <td>08:34</td>
+                <td>17:21</td>
+                <td>
+                ❌
+                </td>
+                <td>
+                    <input type="checkbox" name="" id="">
+                </td>
+            </tr>
+            <tr>
+                <td>EP0352</td>
+                <td>2024-04-03</td>
+                <td>07:54</td>
+                <td>17:35</td>
+                <td>
+                ○
+                </td>
+                <td>
+                    <input type="checkbox" name="" id="">
+                </td>
+            </tr>
+            <tr>
+                <td>EP0352</td>
+                <td>2024-04-04</td>
+                <td>08:17</td>
+                <td>17:11</td>
+                <td>
+                ○
+                </td>
+                <td>
+                    <input type="checkbox" name="" id="">
+                </td>
+            </tr>
+            <tr>
+                <td>EP0352</td>
+                <td>2024-04-05</td>
+                <td>07:46</td>
+                <td>17:40</td>
+                <td>
+                ❌
+                </td>
+                <td>
+                    <input type="checkbox" name="" id="">
+                </td>
+            </tr>
+        </tbody>
     </table>
     <hr>
     新增/更新
+    <div style="background-color:#d9d9d9; width:100%;">
+        <div style="height:50px; line-height:50px;">
+            文字
+            <input type="text">
+            <br/>
+        </div>
+        <div style="height:50px; line-height:50px;">
+            文字
+            <input type="text">
+        </div>
+        <div style="height:50px; line-height:50px;">
+            文字
+            <input type="text">
+            <br/>
+        </div>
+        <div style="height:50px; line-height:50px;">
+            文字
+            <input type="text">
+            <br/>
+        </div>
+    </div>
     <hr>
-    刪除
-    <hr>
+    伸縮選單
+    
 
 </body>
 </html>
--- Manager/View/Nav.php
+++ Manager/View/Nav.php
@@ -5,29 +5,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="./CSS/Nav.css">
+    <style>
+
+        .hidden {
+            display: none; /* 隐藏元素 */
+        }
+
+        .visible {
+            display: block; /* 显示元素 */
+        }
+
+        .Selected_Fun{
+            background-color: #faa9e2;
+        }
+
+    </style>
 </head>
 <body>
     <div id="Company_Logo">
         圖片暫時取代區
     </div>
     <div>
-        <div class="Nav_Block"><a href="">首頁</a><br/></div>
+        <div class="Nav_Block"><button>首頁</button><br/></div>
     </div>
     <div>
-        <div class="Nav_Block">員工管理<br/></div>
+        <div class="Nav_Block"><button onclick="toggleVisibility('EmployFun');">員工管理</button><br/></div>
         <!-- <img style="top:20px;" src="./Img/add_icon.png" /> -->
-        <div>
-            <div class="Nav_Func"><a href="">員工組別</a><br/></div>
-            <div class="Nav_Func"><a href="">員工小組成員管理</a><br/></div>
-            <div class="Nav_Func"><a href="">員工權限</a><br/></div>
-            <div class="Nav_Func"><a href="">員工帳號</a><br/></div>
-            <div class="Nav_Func"><a href="">員工權限列表</a><br/></div>
-            <div class="Nav_Func"><a href="">員工代理人</a><br/></div>
+        <div id="EmployFun" class="visible">
+            <div class="Nav_Func" id="EmployFun1"><button onclick="focusSelectedFunc('EmployFun1');">員工組別</button><br/></div>
+            <div class="Nav_Func" id="EmployFun2"><button onclick="focusSelectedFunc('EmployFun2');">員工小組成員管理</button><br/></div>
+            <div class="Nav_Func" id="EmployFun3"><button onclick="focusSelectedFunc('EmployFun3');">員工權限</button><br/></div>
+            <div class="Nav_Func" id="EmployFun4"><button onclick="focusSelectedFunc('EmployFun4');">員工帳號</button><br/></div>
+            <div class="Nav_Func" id="EmployFun5"><button onclick="focusSelectedFunc('EmployFun5');">員工權限列表</button><br/></div>
+            <div class="Nav_Func" id="EmployFun6"><button onclick="focusSelectedFunc('EmployFun6');">員工代理人</button><br/></div>
         </div>
     </div>
     <div>
-        <div class="Nav_Block">請假管理<br/></div>
-        <div>
+        <div class="Nav_Block"><button onclick="toggleVisibility('LeaveFun');">請假管理</button><br/></div>
+        <div id="LeaveFun" class="visible">
             <div class="Nav_Func"><a href="">請假簽核關卡設定</a><br/></div>
             <div class="Nav_Func"><a href="">請假申請</a><br/></div>
             <div class="Nav_Func"><a href="">請假簽核</a><br/></div>
@@ -45,5 +60,31 @@
 
     </footer>
     <script src="./Javascript/Nav.js"></script>
+
+    <script>
+    function toggleVisibility(element) {
+        var element = document.getElementById(element);
+
+        if (element.classList.contains("visible")) {
+            element.classList.remove("visible");
+            element.classList.add("hidden"); // 添加隐藏类
+        } else {
+            element.classList.remove("hidden");
+            element.classList.add("visible"); // 添加可见类
+        }
+    }
+
+    function focusSelectedFunc(IdName){
+        var elements = document.querySelectorAll('.Nav_Func');
+        var IdName = document.getElementById(IdName);
+
+        elements.forEach(function(element) {
+            element.classList.remove('Selected_Fun');
+        });
+
+        IdName.classList.add("Selected_Fun");
+    }
+
+    </script>
 </body>
 </html>
