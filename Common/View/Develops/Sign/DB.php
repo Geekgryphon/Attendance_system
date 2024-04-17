e68816b4b0662cdb390071ca657298f168d30024
Geekgryphon <geekgryphon@gmail.com>
2024-04-17 17:24:36 +0800

    簽到功能開發測試中

---
+++ Common/View/Develops/Sign/DB.php
@@ -0,0 +1,49 @@
+<?php 
+
+class DB{
+
+    private $host = 'localhost';
+    private $dbname = "AttendanceSystem";
+    private $username = "root";
+    private $password = "12345678";
+
+    private $Con;
+    private $stmt;
+
+    public function __construct(){
+        
+        try {
+            $this->Con = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->username, $this->password);
+            $this->Con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+            // $options = [
+            //     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            //     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            //     PDO::ATTR_EMULATE_PREPARES => false,
+            // ];
+        } catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    } 
+
+    public function query($sql, $arr){
+        $this->stmt = $this->Con->prepare($sql);
+        $this->stmt->execute($arr);
+        return $this->stmt->fetchAll();
+    }
+
+    public function rowCount(){
+        return $this->stmt->rowCount();
+    }
+
+}
+
+
+// DB範例
+// $oDb = new DB();
+// $result = $oDb->query("SELECT * from employee where EmployeeID like :EmployeeID", array(':EmployeeID' => '%EMP%'));
+// print_r($result);
+
+
+?>
